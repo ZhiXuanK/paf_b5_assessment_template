8f22f84c30f54e1386c52a360a8b80c159dbbc53
ZhiXuanK <kangzhixuan@u.nus.edu>
2025-02-13 16:55:22 +0800

    final

--- movies/src/main/java/vttp/batch5/paf/movies/controllers/MainController.java
+++ movies/src/main/java/vttp/batch5/paf/movies/controllers/MainController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.json.JsonArray;
+import jakarta.servlet.http.HttpSession;
 import vttp.batch5.paf.movies.services.MovieService;
 
 @RestController
@@ -21,15 +22,30 @@ public class MainController {
   // TODO: Task 3
   @GetMapping(path = { "/summary" }, produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<String> getProlificDirectors(
-      @RequestParam String count
+      @RequestParam String count,
+      HttpSession sess
   ) {
 
     JsonArray response = movieSvc.getProlificDirectors(Integer.parseInt(count));
 
+    sess.setAttribute("director_table", response);
+
     return ResponseEntity.ok().body(response.toString());
 
   }
-
+/*
   // TODO: Task 4
+  @GetMapping(path={"/summary/pdf"}, produces=MediaType.APPLICATION_PDF_VALUE)
+  public ResponseEntity<String> generatePDF(
+    HttpSession sess
+  ) throws JRException, FileNotFoundException {
+    JsonArray director = (JsonArray) sess.getAttribute("director_table");
+
+    JasperPrint print = movieSvc.generatePDFReport(director);
+
+
+
+    return Response.ok(bytes).type("application/pdf").header("Content-Disposition", "filename=report.pdf").build();
+  }*/
 
 }
--- movies/src/main/java/vttp/batch5/paf/movies/services/MovieService.java
+++ movies/src/main/java/vttp/batch5/paf/movies/services/MovieService.java
@@ -1,13 +1,17 @@
 package vttp.batch5.paf.movies.services;
 
-import java.io.File;
+import java.io.ByteArrayInputStream;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +21,16 @@ import jakarta.json.JsonArrayBuilder;
 import jakarta.json.JsonObject;
 import jakarta.json.JsonObjectBuilder;
 import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
 import net.sf.jasperreports.json.data.JsonDataSource;
+import net.sf.jasperreports.pdf.JRPdfExporter;
+import net.sf.jasperreports.pdf.SimplePdfExporterConfiguration;
+import net.sf.jasperreports.pdf.SimplePdfReportConfiguration;
 import vttp.batch5.paf.movies.repositories.MongoMovieRepository;
 import vttp.batch5.paf.movies.repositories.MySQLMovieRepository;
 
@@ -30,21 +43,27 @@ public class MovieService {
   @Autowired
   private MySQLMovieRepository sqlRepo;
 
+  @Value("${vttp.name}")
+  private String name;
+
+  @Value("${vttp.batch}")
+  private String batch;
+
   // TODO: Task 2
-  
 
   // TODO: Task 3
-  // You may change the signature of this method by passing any number of parameters
+  // You may change the signature of this method by passing any number of
+  // parameters
   // and returning any type
   public JsonArray getProlificDirectors(int n) {
 
     JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
 
-    //get top n directors and list of imdb_id
+    // get top n directors and list of imdb_id
     List<Document> topDirectors = mongoRepo.getTopDirectors(n);
 
-    //using sql, query for revenue and budget of each director
-    for (Document d:topDirectors){
+    // using sql, query for revenue and budget of each director
+    for (Document d : topDirectors) {
 
       JsonObjectBuilder objBuilder = Json.createObjectBuilder();
 
@@ -52,8 +71,8 @@ public class MovieService {
       objBuilder.add("movies_count", d.getInteger("count"));
       List<BigDecimal> revenue = new LinkedList<>();
       List<BigDecimal> budget = new LinkedList<>();
-      //find revenue and budget for each movie id
-      for (String id : d.getList("imdb_id", String.class)){
+      // find revenue and budget for each movie id
+      for (String id : d.getList("imdb_id", String.class)) {
 
         SqlRowSet rs = sqlRepo.getRevenueAndBudget(id);
         rs.next();
@@ -76,15 +95,42 @@ public class MovieService {
 
   }
 
-
   // TODO: Task 4
-  // You may change the signature of this method by passing any number of parameters
+  // You may change the signature of this method by passing any number of
+  // parameters
   // and returning any type
-  public void generatePDFReport() throws FileNotFoundException, JRException {
+  public void generatePDFReport(JsonArray director) throws FileNotFoundException, JRException {
 
-    JsonDataSource reportDS = new JsonDataSource(new File("../data/director_movies_report.jrxml"));
+    JsonObject overall = Json.createObjectBuilder().add("name", name).add("batch", batch).build();
 
-    JsonDataSource directorsDS = new J
+    // Json
+    JsonDataSource reportDS = new JsonDataSource(new ByteArrayInputStream(overall.toString().getBytes()));
+
+    JsonDataSource directorsDS = new JsonDataSource(new ByteArrayInputStream(director.toString().getBytes()));
+
+    Map<String, Object> params = new HashMap<>();
+    params.put("DIRECTOR_TABLE_DATASET", directorsDS);
+
+    InputStream movieReportInputStream = getClass().getResourceAsStream("../director_movies_report.jrxml");
+    JasperReport report = JasperCompileManager.compileReport(movieReportInputStream);
+
+    JasperPrint print = JasperFillManager.fillReport(report, params, reportDS);
+
+    JRPdfExporter exporter = new JRPdfExporter();
+    exporter.setExporterInput(new SimpleExporterInput(print));
+    exporter.setExporterOutput(
+    new SimpleOutputStreamExporterOutput("movieReport.pdf"));
+    SimplePdfReportConfiguration reportConfig = new SimplePdfReportConfiguration();
+    reportConfig.setSizePageToContent(true);
+    reportConfig.setForceLineBreakPolicy(false);
+    SimplePdfExporterConfiguration exportConfig = new SimplePdfExporterConfiguration();
+    exportConfig.setMetadataAuthor("baeldung");
+    exportConfig.setEncrypted(true);
+    exportConfig.setAllowedPermissionsHint("PRINTING");
+    exporter.setConfiguration(reportConfig);
+    exporter.setConfiguration(exportConfig);
+    exporter.exportReport();
+
 
   }
 
